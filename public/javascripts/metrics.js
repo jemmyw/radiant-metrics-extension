2b00ef46974847beaa114990820b7cced4145af8
Jeremy Wells <jeremy@jeremy-laptop.(none)>
2010-05-18 22:32:15 +1200

    added metrics js file

---
+++ public/javascripts/metrics.js
@@ -0,0 +1,54 @@
+// ==ClosureCompiler==
+// @compilation_level ADVANCED_OPTIMIZATIONS
+// @output_file_name default.js
+// ==/ClosureCompiler==
+
+// ADD YOUR CODE HERE
+function sendRequest(url,callback,postData) {
+var req = createXMLHTTPObject();
+if (!req) return;
+var method = (postData) ? "POST" : "GET";
+req.open(method,url,true);
+req.setRequestHeader('User-Agent','XMLHTTP/1.0');
+if (postData)
+req.setRequestHeader('Content-type','application/x-www-form-urlencoded');
+req.onreadystatechange = function () {
+if (req.readyState != 4) return;
+if (req.status != 200 && req.status != 304) {
+//          alert('HTTP error ' + req.status);
+return;
+}
+callback(req);
+}
+if (req.readyState == 4) return;
+req.send(postData);
+}
+
+var XMLHttpFactories = [
+function () {return new XMLHttpRequest()},
+function () {return new ActiveXObject("Msxml2.XMLHTTP")},
+function () {return new ActiveXObject("Msxml3.XMLHTTP")},
+function () {return new ActiveXObject("Microsoft.XMLHTTP")}
+];
+
+function createXMLHTTPObject() {
+var xmlhttp = false;
+for (var i=0;i<XMLHttpFactories.length;i++) {
+try {
+xmlhttp = XMLHttpFactories[i]();
+}
+catch (e) {
+continue;
+}
+break;
+}
+return xmlhttp;
+}
+
+var Metrics = new Object();
+Metrics['track'] = function(metric) {
+sendRequest('/metrics/track/' + metric + '.json', function(){}, null);
+};
+
+window['sendRequest'] = sendRequest;
+window['Metrics'] = Metrics;
