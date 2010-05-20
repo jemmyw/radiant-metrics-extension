c565732479c9de2633f4e3b3c7e0739fcee20170
Jeremy <jeremy@boost.co.nz>
2010-05-20 21:53:04 +1200

    permissions and fix access to track

--- app/controllers/admin/metrics/metrics_controller.rb
+++ app/controllers/admin/metrics/metrics_controller.rb
@@ -1,10 +1,18 @@
 module Admin::Metrics
   class MetricsController < Admin::ResourceController
+    only_allow_access_to :index, :show, :new, :create, :edit, :update, :destroy,
+                         :when => :admin,
+                         :denied_url => {:controller => 'pages', :action => 'index'},
+                         :denied_message => 'You must have administrative privileges to perform this action.'
+    skip_before_filter :authenticate, :only => 'track'
+    skip_before_filter :authorize, :only => 'track'
+
     model_class 'db_metric'
 
     use_vanity
     include Metrics::Identity
 
+    # Use this action to track a metric.
     def track
       id = params[:id].to_sym
       Vanity.playground.track! id
@@ -12,7 +20,13 @@ module Admin::Metrics
       respond_to do |format|
         format.js { render :json => true }
         format.json { render :json => true }
-        format.html { render :nothing => true }
+        format.html do
+          if params[:destination]
+            redirect_to params[:destination]
+          else
+            render :nothing => true
+          end
+        end
       end
     end
   end
--- spec/controllers/metrics_controller_spec.rb
+++ spec/controllers/metrics_controller_spec.rb
@@ -4,4 +4,32 @@ describe Admin::Metrics::MetricsController do
   it 'should include Metrics::Identity' do
     @controller.should be_a(Metrics::Identity)
   end
+
+  describe '#track' do
+    before do
+      Vanity.playground.stub!(:track!)
+    end
+
+    it 'should call track! for the given metric' do
+      Vanity.playground.should_receive(:track!).with(:signup)
+      get :track, :id => 'signup'
+    end
+
+    context 'json' do
+      before { get :track, :id => 'signup', :format => 'json' }
+      its(:response) { should be_success }
+    end
+
+    context 'html' do
+      context 'no destination' do
+        before { get :track, :id => 'signup', :format => 'html' }
+        its(:response) { should be_success }
+      end
+
+      context 'destination specified' do
+        before { get :track, :id => 'signup', :format => 'html', :destination => 'http://example.com' }
+        its(:response) { should redirect_to 'http://example.com' }
+      end
+    end
+  end
 end
