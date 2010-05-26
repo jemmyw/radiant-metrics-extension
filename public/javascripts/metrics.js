49a080e9e93ed4808035cc87451cfbbb3b63fb1e
Jeremy <jeremy@boost.co.nz>
2010-05-26 17:58:28 +1200

    format metrics.js

--- public/javascripts/metrics.js
+++ public/javascripts/metrics.js
@@ -4,50 +4,59 @@
 // ==/ClosureCompiler==
 
 // ADD YOUR CODE HERE
-function sendRequest(url,callback,postData) {
-var req = createXMLHTTPObject();
-if (!req) return;
-var method = (postData) ? "POST" : "GET";
-req.open(method,url,true);
-req.setRequestHeader('User-Agent','XMLHTTP/1.0');
-if (postData)
-req.setRequestHeader('Content-type','application/x-www-form-urlencoded');
-req.onreadystatechange = function () {
-if (req.readyState != 4) return;
-if (req.status != 200 && req.status != 304) {
-//          alert('HTTP error ' + req.status);
-return;
-}
-callback(req);
-}
-if (req.readyState == 4) return;
-req.send(postData);
+function sendRequest(url, callback, postData) {
+  var req = createXMLHTTPObject();
+  if (!req) return;
+  var method = (postData) ? "POST" : "GET";
+  req.open(method, url, true);
+  req.setRequestHeader('User-Agent', 'XMLHTTP/1.0');
+  if (postData)
+    req.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+  req.onreadystatechange = function () {
+    if (req.readyState != 4) return;
+    if (req.status != 200 && req.status != 304) {
+      //          alert('HTTP error ' + req.status);
+      return;
+    }
+    callback(req);
+  }
+  if (req.readyState == 4) return;
+  req.send(postData);
 }
 
 var XMLHttpFactories = [
-function () {return new XMLHttpRequest()},
-function () {return new ActiveXObject("Msxml2.XMLHTTP")},
-function () {return new ActiveXObject("Msxml3.XMLHTTP")},
-function () {return new ActiveXObject("Microsoft.XMLHTTP")}
+  function () {
+    return new XMLHttpRequest()
+  },
+  function () {
+    return new ActiveXObject("Msxml2.XMLHTTP")
+  },
+  function () {
+    return new ActiveXObject("Msxml3.XMLHTTP")
+  },
+  function () {
+    return new ActiveXObject("Microsoft.XMLHTTP")
+  }
 ];
 
 function createXMLHTTPObject() {
-var xmlhttp = false;
-for (var i=0;i<XMLHttpFactories.length;i++) {
-try {
-xmlhttp = XMLHttpFactories[i]();
-}
-catch (e) {
-continue;
-}
-break;
-}
-return xmlhttp;
+  var xmlhttp = false;
+  for (var i = 0; i < XMLHttpFactories.length; i++) {
+    try {
+      xmlhttp = XMLHttpFactories[i]();
+    }
+    catch (e) {
+      continue;
+    }
+    break;
+  }
+  return xmlhttp;
 }
 
 var Metrics = new Object();
 Metrics['track'] = function(metric) {
-sendRequest('/metrics/track/' + metric + '.json', function(){}, null);
+  sendRequest('/metrics/track/' + metric + '.json', function() {
+  }, null);
 };
 
 window['sendRequest'] = sendRequest;
