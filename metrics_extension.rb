8c0984f62aa7f33b95083289716ffbb3b0c7c0f7
Jeremy <jemmyw@gmail.com>
2010-09-19 13:25:21 +1200

    version bump

--- metrics_extension.rb
+++ metrics_extension.rb
@@ -2,7 +2,7 @@
 require_dependency 'application_controller'
 
 class MetricsExtension < Radiant::Extension
-  version "1.0.2"
+  version "1.1.0"
   description "Metrics for Radiant"
   url "http://github.com/jemmyw/radiant-metrics-extension"
 
